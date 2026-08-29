da3e2bd70d6f858f7f41874f7907ff15028acb35
INAPP <imapp.help@gmal.com>
2026-08-29 10:55:45 -0700

    fix: use printf+sed instead of awk in clickhouse entrypoint

--- services/clickhouse/docker-entrypoint.sh
+++ services/clickhouse/docker-entrypoint.sh
@@ -6,46 +6,35 @@ mkdir -p /var/lib/clickhouse/coordination/log
 mkdir -p /var/lib/clickhouse/coordination/snapshots
 chown -R clickhouse:clickhouse /var/lib/clickhouse/coordination
 
-# Generate users.xml from environment variables using awk (no python needed)
+# Generate users.xml from environment variables using printf (bash builtin)
 USER_NAME="${CLICKHOUSE_USER:-default}"
 USER_PASSWORD="${CLICKHOUSE_PASSWORD:-}"
 
-# Use awk to safely generate XML with proper escaping
-awk -v user="$USER_NAME" -v pw="$USER_PASSWORD" 'BEGIN {
-    # XML-escape the password
-    gsub(/&/, "\\&amp;", pw)
-    gsub(/</, "\\&lt;", pw)
-    gsub(/>/, "\\&gt;", pw)
-    gsub(/"/, "\\&quot;", pw)
-    gsub(/\x27/, "\\&apos;", pw)
-    # XML-escape the username
-    gsub(/&/, "\\&amp;", user)
-    gsub(/</, "\\&lt;", user)
-    gsub(/>/, "\\&gt;", user)
-    gsub(/"/, "\\&quot;", user)
-    gsub(/\x27/, "\\&apos;", user)
-    printf "<?xml version=\"1.0\"?>\n"
-    printf "<clickhouse>\n"
-    printf "    <profiles>\n"
-    printf "        <default>\n"
-    printf "            <max_memory_usage>10000000000</max_memory_usage>\n"
-    printf "            <use_uncompressed_cache>0</use_uncompressed_cache>\n"
-    printf "            <load_balancing>random</load_balancing>\n"
-    printf "        </default>\n"
-    printf "    </profiles>\n"
-    printf "    <users>\n"
-    printf "        <%s>\n", user
-    printf "            <password>%s</password>\n", pw
-    printf "            <networks>\n"
-    printf "                <ip>::/0</ip>\n"
-    printf "            </networks>\n"
-    printf "            <profile>default</profile>\n"
-    printf "            <quota>default</quota>\n"
-    printf "            <access_management>1</access_management>\n"
-    printf "        </%s>\n", user
-    printf "    </users>\n"
-    printf "</clickhouse>\n"
-}' > /etc/clickhouse-server/users.xml
+# XML-escape the password (using sed which is a coreutil)
+esc_pw=$(printf '%s' "$USER_PASSWORD" | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&apos;/g')
+esc_user=$(printf '%s' "$USER_NAME" | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&apos;/g')
+
+printf '<?xml version="1.0"?>\n' > /etc/clickhouse-server/users.xml
+printf '<clickhouse>\n' >> /etc/clickhouse-server/users.xml
+printf '    <profiles>\n' >> /etc/clickhouse-server/users.xml
+printf '        <default>\n' >> /etc/clickhouse-server/users.xml
+printf '            <max_memory_usage>10000000000</max_memory_usage>\n' >> /etc/clickhouse-server/users.xml
+printf '            <use_uncompressed_cache>0</use_uncompressed_cache>\n' >> /etc/clickhouse-server/users.xml
+printf '            <load_balancing>random</load_balancing>\n' >> /etc/clickhouse-server/users.xml
+printf '        </default>\n' >> /etc/clickhouse-server/users.xml
+printf '    </profiles>\n' >> /etc/clickhouse-server/users.xml
+printf '    <users>\n' >> /etc/clickhouse-server/users.xml
+printf '        <%s>\n' "$esc_user" >> /etc/clickhouse-server/users.xml
+printf '            <password>%s</password>\n' "$esc_pw" >> /etc/clickhouse-server/users.xml
+printf '            <networks>\n' >> /etc/clickhouse-server/users.xml
+printf '                <ip>::/0</ip>\n' >> /etc/clickhouse-server/users.xml
+printf '            </networks>\n' >> /etc/clickhouse-server/users.xml
+printf '            <profile>default</profile>\n' >> /etc/clickhouse-server/users.xml
+printf '            <quota>default</quota>\n' >> /etc/clickhouse-server/users.xml
+printf '            <access_management>1</access_management>\n' >> /etc/clickhouse-server/users.xml
+printf '        </%s>\n' "$esc_user" >> /etc/clickhouse-server/users.xml
+printf '    </users>\n' >> /etc/clickhouse-server/users.xml
+printf '</clickhouse>\n' >> /etc/clickhouse-server/users.xml
 
 chown clickhouse:clickhouse /etc/clickhouse-server/users.xml
 
