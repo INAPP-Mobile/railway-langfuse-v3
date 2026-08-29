89323a031a73d4bc2f73589fe8876e5db88d0544
INAPP <imapp.help@gmal.com>
2026-08-29 10:50:28 -0700

    fix: use awk instead of python3 in clickhouse entrypoint (python3 not in image)

--- services/clickhouse/docker-entrypoint.sh
+++ services/clickhouse/docker-entrypoint.sh
@@ -6,36 +6,46 @@ mkdir -p /var/lib/clickhouse/coordination/log
 mkdir -p /var/lib/clickhouse/coordination/snapshots
 chown -R clickhouse:clickhouse /var/lib/clickhouse/coordination
 
-# Generate users.xml from environment variables
-python3 -c "
-import os, sys
-user = os.environ.get('CLICKHOUSE_USER', 'default')
-pw = os.environ.get('CLICKHOUSE_PASSWORD', '')
-xml = '''<?xml version=\"1.0\"?>
-<clickhouse>
-    <profiles>
-        <default>
-            <max_memory_usage>10000000000</max_memory_usage>
-            <use_uncompressed_cache>0</use_uncompressed_cache>
-            <load_balancing>random</load_balancing>
-        </default>
-    </profiles>
-    <users>
-        <''' + user + '''>
-            <password>''' + pw + '''</password>
-            <networks>
-                <ip>::/0</ip>
-            </networks>
-            <profile>default</profile>
-            <quota>default</quota>
-            <access_management>1</access_management>
-        </''' + user + '''>
-    </users>
-</clickhouse>
-'''
-with open('/etc/clickhouse-server/users.xml', 'w') as f:
-    f.write(xml)
-"
+# Generate users.xml from environment variables using awk (no python needed)
+USER_NAME="${CLICKHOUSE_USER:-default}"
+USER_PASSWORD="${CLICKHOUSE_PASSWORD:-}"
+
+# Use awk to safely generate XML with proper escaping
+awk -v user="$USER_NAME" -v pw="$USER_PASSWORD" 'BEGIN {
+    # XML-escape the password
+    gsub(/&/, "\\&amp;", pw)
+    gsub(/</, "\\&lt;", pw)
+    gsub(/>/, "\\&gt;", pw)
+    gsub(/"/, "\\&quot;", pw)
+    gsub(/\x27/, "\\&apos;", pw)
+    # XML-escape the username
+    gsub(/&/, "\\&amp;", user)
+    gsub(/</, "\\&lt;", user)
+    gsub(/>/, "\\&gt;", user)
+    gsub(/"/, "\\&quot;", user)
+    gsub(/\x27/, "\\&apos;", user)
+    printf "<?xml version=\"1.0\"?>\n"
+    printf "<clickhouse>\n"
+    printf "    <profiles>\n"
+    printf "        <default>\n"
+    printf "            <max_memory_usage>10000000000</max_memory_usage>\n"
+    printf "            <use_uncompressed_cache>0</use_uncompressed_cache>\n"
+    printf "            <load_balancing>random</load_balancing>\n"
+    printf "        </default>\n"
+    printf "    </profiles>\n"
+    printf "    <users>\n"
+    printf "        <%s>\n", user
+    printf "            <password>%s</password>\n", pw
+    printf "            <networks>\n"
+    printf "                <ip>::/0</ip>\n"
+    printf "            </networks>\n"
+    printf "            <profile>default</profile>\n"
+    printf "            <quota>default</quota>\n"
+    printf "            <access_management>1</access_management>\n"
+    printf "        </%s>\n", user
+    printf "    </users>\n"
+    printf "</clickhouse>\n"
+}' > /etc/clickhouse-server/users.xml
 
 chown clickhouse:clickhouse /etc/clickhouse-server/users.xml
 
